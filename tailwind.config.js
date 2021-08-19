be89a76d2cdc5e0cc77fbebeb0ae95de6f588ecf
Donato Alvarez <chatblack1999@gmail.com>
2021-08-19 12:16:47 -0700

    [ADDED] using tailwind custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-
+    colors: {
+      pink_red: '#EF2057',
+      light_blue: '#93D1FF',
+      light_violet: '#D3ABF7',
+      light_yellow: '#FFC871'
+    },
     extend: {},
   },
   variants: {
