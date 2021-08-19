601eae967f348200cbb3c6a7a4de07f05770c04b
Donato Alvarez <chatblack1999@gmail.com>
2021-08-19 11:50:33 -0700

    first commit

---
+++ src/app/App.js
@@ -0,0 +1,11 @@
+import Directory from "../pages/Directory";
+
+function App() {
+  return (
+    <div className="App">
+      <Directory />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Directory.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Directory = () => {
+  return (
+    <>
+      <h1 className="">hola</h1>
+    </>
+  );
+};
+
+export default Directory;
