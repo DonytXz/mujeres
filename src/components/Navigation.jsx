41a58ad30cb0c47e3b3408da43b7b2715dbd0490
Donato Alvarez <chatblack1999@gmail.com>
2021-08-19 20:40:37 -0500

    [UPDATED] font size and weight

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,12 +5,12 @@ const Navigation = () => {
   return (
     <>
       <div className="flex flex-row">
-        <div className="px-2 my-auto text-white">Inicio</div>
-        <div className="px-2 my-auto text-white">Ingresar</div>
-        <div className="px-2 my-auto text-white">Contacto</div>
-        <div className="flex flex-row px-2 my-auto text-white">
+        <div className="px-2 my-auto text-white text-xl font-bold">Inicio</div>
+        <div className="px-2 my-auto text-white text-xl font-bold">Ingresar</div>
+        <div className="px-2 my-auto text-white text-xl font-bold">Contacto</div>
+        <div className="flex flex-row px-2 my-auto">
           <img className="px-2" src={Icon} alt="" srcSet="" />
-          <p className="px-2 my-auto text-white">Directorio Digital</p>
+          <p className="px-2 my-auto text-white text-xl font-bold">Directorio Digital</p>
         </div>
       </div>
     </>
--- src/components/directory/Card.jsx
+++ src/components/directory/Card.jsx
@@ -16,8 +16,8 @@ const Card = (props) => {
           </div>
 
           <div className=" flex flex-col">
-            <div className="text-white">{subtitle}</div>
-            <div className="text-white">{title}</div>
+            <div className="text-white text-base font-bold">{subtitle}</div>
+            <div className="text-white text-xl font-bold">{title}</div>
           </div>
           <img
             className="absolute -bottom-10 justify-self-end"
@@ -28,22 +28,22 @@ const Card = (props) => {
         </div>
         <div className="p-4 flex flex-col">
           <div className="w-full">
-            <p className={`text-${color}`}>{place}</p>
+            <p className={`text-xl font-bold text-${color}`}>{place}</p>
           </div>
           <div className="w-full">
-            <p>Gudalajara</p>
+            <p className="text-xs font-semibold my-2 text-gray-700">Gudalajara</p>
             <div className="flex flex-col">
               <div className="flex flex-row">
                 <img src={incDirection} alt="" srcSet="" />
-                <p className="ml-4">Miguel Gordoa #1782 Int 2 Col. Guadalupana Sur</p>
+                <p className="ml-4 text-xs font-medium text-gray-500">Miguel Gordoa #1782 Int 2 Col. Guadalupana Sur</p>
               </div>
               <div className="flex flex-row">
                 <img src={incDirection} alt="" srcSet="" />
-                <p className="ml-4">(333) 31 27 16 54 / (333) 35 43 52 72</p>
+                <p className="ml-4 text-xs font-bold text-gray-500">(333) 31 27 16 54 / (333) 35 43 52 72</p>
               </div>
               <div className="flex flex-row">
                 <img src={incDirection} alt="" srcSet="" />
-                <p className="ml-4">nutrikarinagut@gmail.com</p>
+                <p className="ml-4 text-xs font-medium text-gray-500">nutrikarinagut@gmail.com</p>
               </div>
             </div>
           </div>
@@ -51,15 +51,15 @@ const Card = (props) => {
             <div className="flex flex-row">
               <img src={incFb} alt="" srcset="" />
               <div className="flex flex-col ml-4">
-                <div>Facebook</div>
-                <div>Nutrikarinagut</div>
+                <div className="text-xs text-gray-400 font-bold">Facebook</div>
+                <div className="text-xs text-gray-400 font-bold">Nutrikarinagut</div>
               </div>
             </div>
             <div className="flex flex-row">
               <img src={incInsta} alt="" srcset="" />
               <div className="flex flex-col ml-4">
-                <div>Instagram</div>
-                <div>Nutrikarinagut</div>
+                <div className="text-xs text-gray-400 font-bold">Instagram</div>
+                <div className="text-xs text-gray-400 font-bold">Nutrikarinagut</div>
               </div>
             </div>
           </div>
