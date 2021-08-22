001d54b4e60f67d35c9474aae4e646408a358671
Donato Alvarez <chatblack1999@gmail.com>
2021-08-22 17:10:18 -0500

    [UPDATED] styling card

--- src/components/directory/Card.jsx
+++ src/components/directory/Card.jsx
@@ -7,9 +7,9 @@ const Card = (props) => {
   const { title, subtitle, color, imgPath1, imgPath2, place } = props;
   return (
     <>
-      <div className="w-full h-full border-2 border-transparent rounded-2xl shadow-lg">
+      <div className="w-full h-full border-2 border-transparent rounded-2xl card_shadow flex flex-col">
         <div
-          className={`bg-${color} grid grid-fol-col grid-cols-3 border-t-2 border-transparent rounded-t-2xl p-2 relative`}
+          className={`bg-${color} grid grid-fol-col grid-cols-3 border-t-2 border-transparent rounded-t-2xl px-2 py-4 relative`}
         >
           <div className="">
             <img height="58" width="58" src={imgPath1} alt="" srcSet="" />
@@ -22,34 +22,34 @@ const Card = (props) => {
           <img
             height="110"
             width="110"
-            className="absolute -bottom-9 justify-self-end"
+            className="absolute -bottom-6 justify-self-end"
             src={imgPath2}
             alt=""
             srcSet=""
           />
         </div>
-        <div className="p-4 flex flex-col">
-          <div className="w-full">
-            <p className={`text-xl font-bold text-${color}`}>{place}</p>
-          </div>
+        <div className="px-4 w-full my-auto">
+          <p className={`text-xl font-bold text-${color}`}>{place}</p>
+        </div>
+        <div className="px-4 pb-4 flex flex-col self-end">
           <div className="w-full">
             <p className="text-xs font-semibold my-2 text-gray-700">
               Gudalajara
             </p>
             <div className="flex flex-col">
-              <div className="flex flex-row">
+              <div className="flex flex-row my-1">
                 <img src={incDirection} alt="" srcSet="" />
                 <p className="ml-4 text-xs font-medium text-gray-500">
                   Miguel Gordoa #1782 Int 2 Col. Guadalupana Sur
                 </p>
               </div>
-              <div className="flex flex-row">
+              <div className="flex flex-row my-1">
                 <img src={incDirection} alt="" srcSet="" />
                 <p className="ml-4 text-xs font-bold text-gray-500">
                   (333) 31 27 16 54 / (333) 35 43 52 72
                 </p>
               </div>
-              <div className="flex flex-row">
+              <div className="flex flex-row my-1">
                 <img src={incDirection} alt="" srcSet="" />
                 <p className="ml-4 text-xs font-medium text-gray-500">
                   nutrikarinagut@gmail.com
@@ -57,8 +57,8 @@ const Card = (props) => {
               </div>
             </div>
           </div>
-          <div className="w-full flex fle-row justify-between border-t-2 mt-4 border-gray-150">
-            <div className="flex flex-row">
+          <div className="w-full flex fle-row justify-between border-t-2 mt-2 border-gray-150">
+            <div className="flex flex-row mt-2">
               <img src={incFb} alt="" srcset="" />
               <div className="flex flex-col ml-4">
                 <div className="text-xs text-gray-400 font-bold">Facebook</div>
@@ -67,7 +67,7 @@ const Card = (props) => {
                 </div>
               </div>
             </div>
-            <div className="flex flex-row">
+            <div className="flex flex-row mt-2">
               <img src={incInsta} alt="" srcset="" />
               <div className="flex flex-col ml-4">
                 <div className="text-xs text-gray-400 font-bold">Instagram</div>
